1bc3fe18414cf0bbdea0d4c356487ea8136db797
TENMA <171513197+ASHEN-VECTOR@users.noreply.github.com>
2024-08-08 16:23:49 -0500

    Create LinuxRPG Project

---
+++ LinuxRPG.cpp
@@ -0,0 +1,14 @@
+#include iostream // For input/output operations
+#include strinf // For string manipulation
+#include vector // For dynamic arrays
+#include map // For key-value pair storage
+#include algorithm // for various algorithms like sorting
+#include sstream // for string stream processing
+
+//The main function and entry point of my program 
+int main(){
+    //Prints a greeting to the user
+    std::cout << "Welcome to the LinuxRPG!" << std::endl;
+    // Return a 0 to indicate succesful program execution
+    return 0;
+}
