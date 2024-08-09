8fc452c6d7e563c59fb0ab7db245d7d4fe08626b
TENMA <171513197+ASHEN-VECTOR@users.noreply.github.com>
2024-08-08 21:38:19 -0500

    Created a Structure for Nodes Nodes have been given properties

--- LinuxRPG.cpp
+++ LinuxRPG.cpp
@@ -5,6 +5,20 @@
 #include <algorithm> // for various algorithms like sorting
 #include <sstream> // for string stream processing
 
+//Defining the FileSystem class that will become our main game world
+class FileSystem {
+public:
+    // Defining a Structure. The Structure will create nodes (files or directories) that populate inside the FileSystems our Constructor makes
+    struct FSNode{
+        std::string type;        // Can be a "file" or "directory"
+        std::string description; // A brief description of the node
+        std::string content;    // Content of the file (currently empty for directories)
+        bool hidden = false;    // Whether the node is hidden from the user or not
+    };
+
+
+};
+
 //The main function and entry point of my program 
 int main(){
     //Prints a greeting to the user
